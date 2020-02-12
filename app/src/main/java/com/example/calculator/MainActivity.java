f264cc2c1133216d1d821d2313e83d79e85740eb
naufalnur <naufalnuraidil012@gmail.com>
2020-02-12 14:40:58 +0700

    Calculator has been completed

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -10,34 +10,34 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnJml, btnKrg, btnKali, btnBagi;
+    private Button btnCos, btnSec, btnTan, btnCsc, btnCtn, btnSin, btnLog;
     private TextView tvHasil;
-    private EditText etPanjang, etLebar;
+    private EditText etInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnJml = findViewById(R.id.jumlah);
-        btnKrg = findViewById(R.id.kurang);
-        btnKali = findViewById(R.id.kali);
-        btnBagi = findViewById(R.id.bagi);
+        btnCos = findViewById(R.id.cos);
+        btnSin = findViewById(R.id.sin);
+        btnSec = findViewById(R.id.sec);
+        btnCsc = findViewById(R.id.cosec);
+        btnTan = findViewById(R.id.tan);
+        btnCtn = findViewById(R.id.cotan);
+        btnLog = findViewById(R.id.logaritma);
         tvHasil = findViewById(R.id.hasil);
-        etPanjang = findViewById(R.id.line1);
-        etLebar = findViewById(R.id.line2);
+        etInput = findViewById(R.id.line);
 
-        btnJml.setOnClickListener(new View.OnClickListener() {
+        btnCos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    String sPanjang = etPanjang.getText().toString();
-                    String sLebar = etLebar.getText().toString();
+                    String sInput= etInput.getText().toString();
 
-                    double panjang = Double.parseDouble(sPanjang);
-                    double lebar = Double.parseDouble(sLebar);
+                    double input = Double.parseDouble(sInput);
 
-                    double hasil = panjang + lebar;
+                    double hasil = Math.cos(input);
                     String sHasil = String.valueOf(hasil);
                     tvHasil.setText(sHasil);
                 } catch (NumberFormatException nfe) {
@@ -46,17 +46,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnKrg.setOnClickListener(new View.OnClickListener() {
+        btnSin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    String sPanjang = etPanjang.getText().toString();
-                    String sLebar = etLebar.getText().toString();
+                    String sInput= etInput.getText().toString();
 
-                    double panjang = Double.parseDouble(sPanjang);
-                    double lebar = Double.parseDouble(sLebar);
+                    double input = Double.parseDouble(sInput);
 
-                    double hasil = panjang - lebar;
+                    double hasil = Math.sin(input);;
                     String sHasil = String.valueOf(hasil);
                     tvHasil.setText(sHasil);
                 } catch (NumberFormatException nfe) {
@@ -65,17 +63,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnKali.setOnClickListener(new View.OnClickListener() {
+        btnTan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    String sPanjang = etPanjang.getText().toString();
-                    String sLebar = etLebar.getText().toString();
+                    String sInput= etInput.getText().toString();
 
-                    double panjang = Double.parseDouble(sPanjang);
-                    double lebar = Double.parseDouble(sLebar);
+                    double input = Double.parseDouble(sInput);
 
-                    double hasil = panjang * lebar;
+                    double hasil = Math.tan(input);
                     String sHasil = String.valueOf(hasil);
                     tvHasil.setText(sHasil);
                 } catch (NumberFormatException nfe) {
@@ -84,17 +80,66 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnBagi.setOnClickListener(new View.OnClickListener() {
+        btnCsc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    String sPanjang = etPanjang.getText().toString();
-                    String sLebar = etLebar.getText().toString();
+                    String sInput= etInput.getText().toString();
 
-                    double panjang = Double.parseDouble(sPanjang);
-                    double lebar = Double.parseDouble(sLebar);
+                    double input = Double.parseDouble(sInput);
 
-                    double hasil = panjang / lebar;
+                    double hasil = 1/Math.sin(input);
+                    String sHasil = String.valueOf(hasil);
+                    tvHasil.setText(sHasil);
+                } catch (NumberFormatException nfe) {
+                    Toast.makeText(getApplicationContext(), "Field tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnSec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String sInput= etInput.getText().toString();
+
+                    double input = Double.parseDouble(sInput);
+
+                    double hasil = 1/Math.cos(input);
+                    String sHasil = String.valueOf(hasil);
+                    tvHasil.setText(sHasil);
+                } catch (NumberFormatException nfe) {
+                    Toast.makeText(getApplicationContext(), "Field tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnCtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String sInput= etInput.getText().toString();
+
+                    double input = Double.parseDouble(sInput);
+
+                    double hasil = 1/Math.tan(input);
+                    String sHasil = String.valueOf(hasil);
+                    tvHasil.setText(sHasil);
+                } catch (NumberFormatException nfe) {
+                    Toast.makeText(getApplicationContext(), "Field tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String sInput= etInput.getText().toString();
+
+                    double input = Double.parseDouble(sInput);
+
+                    double hasil = Math.log10(input);
                     String sHasil = String.valueOf(hasil);
                     tvHasil.setText(sHasil);
                 } catch (NumberFormatException nfe) {
